d09f2bb1e765ffc6e39690e417c33c680f50bd6c
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-12-16 07:05:53 -0500

    Add files via upload

---
+++ app/(home)/hotel.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const hotel = () => {
+  return (
+    <View>
+      <Text>Hotel</Text>
+    </View>
+  );
+};
+
+export default hotel;
+
+const styles = StyleSheet.create({});
