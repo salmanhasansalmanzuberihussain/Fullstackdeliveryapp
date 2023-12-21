8588d137cec8cc93ae1727f10390440e68adbe68
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-12-21 11:10:18 -0500

    Add files via upload

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -1,6 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { AntDesign } from '@expo/vector-icons';
+import {AntDesign} from '@expo/vector-icons'
 
 const FoodItem = ({ item }) => {
   const data = [item];
@@ -8,22 +8,14 @@ const FoodItem = ({ item }) => {
     <View>
       {data?.map((item, index) => (
         <>
-          <Pressable
-            style={{
-              margin: 10,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}
-            key={index}
-          >
-            <Text>
-              {item?.name} ({item?.items?.length}){' '}
-            </Text>
-            <AntDesign name="down" size={20} color="black" />
-          </Pressable>
-          {item?.items?.map((item, index) => (
-            <MenuItem key={index} item={item} />
+        <Pressable style={{margin:10, flexDirection:"row", alignItems:"center", justifyContent:"space-between" }} key={index}>
+          <Text>
+            {item?.name} ({item?.items?.length}){' '}
+          </Text>
+          <AntDesign name="down" size={20} color="black"/>
+        </Pressable>
+          {item?.items?.map((item,index)=> (
+            <MenuItem key={index} item={item}/>
           ))}
         </>
       ))}
